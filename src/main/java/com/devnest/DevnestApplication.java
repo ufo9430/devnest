2924690ebbc535c8a2f5ecdf0698d84d768d3798
ufo9430 <ufo9430@naver.com>
2025-06-26 12:17:38 +0900

    choir: initialize Spring Boot project with Maven

---
+++ src/main/java/com/devnest/DevnestApplication.java
@@ -0,0 +1,13 @@
+package com.devnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevnestApplication.class, args);
+	}
+
+}
