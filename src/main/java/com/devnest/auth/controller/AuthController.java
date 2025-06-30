da7eee4e26e1962d94c3591e192de32af2c90174
Lee Chae Rin <chaerin0916@gmail.com>
2025-06-30 17:53:41 +0900

    feat: Authentication으로 user 정보 가져오도록 수정

--- src/main/java/com/devnest/auth/controller/AuthController.java
+++ src/main/java/com/devnest/auth/controller/AuthController.java
@@ -1,23 +1,17 @@
-package com.devnest.user.controller;
+package com.devnest.auth.controller;
 
-import com.devnest.user.dto.common.ApiResponse;
 import com.devnest.user.dto.request.LoginRequestDto;
 import com.devnest.user.dto.request.RegisterRequestDto;
-import com.devnest.user.dto.response.LoginResponseDto;
-import com.devnest.user.dto.response.RegisterResponseDto;
 import com.devnest.user.service.MemberLoginService;
 import com.devnest.user.service.MemberRegisterService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -46,9 +40,16 @@ public class AuthController {
   }
 
   @PostMapping("/login")
-  public String processLogin(@ModelAttribute @Valid LoginRequestDto requestDto, HttpSession session, Model model) {
-    memberLoginService.login(requestDto, session);
-    return "redirect:/";
+  public String processLogin(@ModelAttribute @Valid LoginRequestDto requestDto, HttpSession session,
+      Model model) {
+    try {
+      memberLoginService.login(requestDto, session);
+      return "redirect:/";
+    } catch (IllegalArgumentException e) {
+      model.addAttribute("active_error", e.getMessage());
+      return "login"; // 로그인 페이지로 다시 이동
+    }
+
   }
 
   @PostMapping("/logout")
@@ -58,7 +59,7 @@ public class AuthController {
   }
 
   @GetMapping("/find-account")
-  public String showFindAccountForm(){
+  public String showFindAccountForm() {
     return "find_account";
   }
 
--- src/main/java/com/devnest/user/controller/TempController.java
+++ src/main/java/com/devnest/user/controller/TempController.java
@@ -1,7 +1,10 @@
 package com.devnest.user.controller;
 
-import jakarta.servlet.http.HttpSession;
+import com.devnest.auth.domain.CustomUserDetails;
+import com.devnest.user.service.MemberLoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +15,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class TempController {
 
+  private final MemberLoginService memberLoginService;
+
   @GetMapping()
-  public String showHome(HttpSession session, Model model) {
-    Object user = session.getAttribute("LOGIN_USER"); // 로그인 시 세션에 저장한 이름으로 가져오기
-    if (user != null) {
-      System.out.println("Session User: " + user);
+  public String showHome(Model model) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication != null && authentication.isAuthenticated()
+        && !(authentication.getPrincipal() instanceof String)) { // anonymousUser 예외 처리
+
+      CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+      Long userId = userDetails.getUserId();
+      String email = userDetails.getEmail();
+      System.out.println("userId = " + userId + ", email = " + email);
+      model.addAttribute("sessionUser", userDetails);
+
+      if (userDetails.getAuthorities().stream()
+          .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+        return "redirect:/admin";
+      }
     } else {
       System.out.println("로그인된 사용자 없음");
+      model.addAttribute("sessionUser", null);
     }
-    model.addAttribute("sessionUser", user);
+
     return "index";
   }
 
