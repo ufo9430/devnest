0b76415ba5089033c0757bacab1bb07fbcdfd4a6
ufo9430 <ufo9430@naver.com>
2025-07-02 14:35:51 +0900

    style: 리포지토리, 서비스 이름 변경

--- src/main/java/com/devnest/board/controller/IndexController.java
+++ src/main/java/com/devnest/board/controller/IndexController.java
@@ -3,8 +3,8 @@ package com.devnest.board.controller;
 import com.devnest.board.domain.HotTopic;
 import com.devnest.board.domain.Tag;
 import com.devnest.board.domain.Topic;
-import com.devnest.board.service.BoardIndexService;
-import com.devnest.board.service.BoardTagService;
+import com.devnest.board.service.TopicService;
+import com.devnest.board.service.TagService;
 import com.devnest.board.service.HotTopicService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,25 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-public class BoardIndexController {
+public class IndexController {
     @Autowired
-    private BoardIndexService boardIndexService;
+    private TopicService topicService;
     @Autowired
     private HotTopicService hotTopicService;
     @Autowired
-    private BoardTagService boardTagService;
+    private TagService tagService;
 
 
     @RequestMapping("/")
     public String index(Model model, HttpSession session){
 
-        List<Topic> recentTopics = boardIndexService.getRecentFiveTopics();
+        List<Topic> recentTopics = topicService.getRecentFiveTopics();
         model.addAttribute("recent",recentTopics);
 
         List<HotTopic> recentHotTopics = hotTopicService.getRecentFiveHotTopics();
         model.addAttribute("hot",recentHotTopics);
 
-        List<Tag> allTags = boardTagService.findAll();
+        List<Tag> allTags = tagService.findAll();
         model.addAttribute("tags",allTags);
 
         //임시 세션 부여
--- src/main/java/com/devnest/board/service/TopicService.java
+++ src/main/java/com/devnest/board/service/TopicService.java
@@ -1,23 +1,27 @@
 package com.devnest.board.service;
 
 import com.devnest.board.domain.Topic;
-import com.devnest.board.repository.BoardTopicRepository;
+import com.devnest.board.repository.TopicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class BoardIndexService {
-    private final BoardTopicRepository topicRepository;
+public class TopicService {
+    private final TopicRepository topicRepository;
 
     @Autowired
-    public BoardIndexService(BoardTopicRepository topicRepository) {
+    public TopicService(TopicRepository topicRepository) {
         this.topicRepository = topicRepository;
     }
 
     public List<Topic> getRecentFiveTopics(){
         return topicRepository.findRecentFiveTopics();
+    }
+
+    public List<Topic> getRecentTopics(){
+        return topicRepository.findAll();
     }
 
 }
