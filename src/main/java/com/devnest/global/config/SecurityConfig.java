1767f7b3f559f61ce20d20a35b204638570da2b6
rktclgh <songchih@icloud.com>
2025-07-02 10:01:31 +0900

    EmailSender -> 임시 비밀번호를 메일로 전송하는 메소드 추가 PasswordResetService -> 임시비밀번호 인증, 발급 하는 메소드 Dto, 요청, 응답 규격

--- src/main/java/com/devnest/global/config/SecurityConfig.java
+++ src/main/java/com/devnest/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,8 +14,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity  // Spring Security 필터 체인 활성화
 @RequiredArgsConstructor
 public class SecurityConfig {
-  private final CustomUserDetailsService userDetailsService;
-  private final PasswordEncoder passwordEncoder;
+    private final CustomUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
