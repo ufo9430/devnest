71e260c5ad8b7547b1de1fde55f50868f443cece
ufo9430 <ufo9430@naver.com>
2025-07-03 13:44:37 +0900

    fix: 도메인 매핑 오타 수정

--- src/main/java/com/devnest/board/domain/BoardAnswer.java
+++ src/main/java/com/devnest/board/domain/BoardAnswer.java
@@ -3,10 +3,12 @@ package com.devnest.board.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "answer")
 public class BoardAnswer {
     @Id
     @Column(name = "answer_id")
