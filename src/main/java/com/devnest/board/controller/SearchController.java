e447fa71364128585ca1c7e089287114cf72bbe3
ufo9430 <ufo9430@naver.com>
2025-07-04 13:42:32 +0900

    feat: 태그 검색 기능 구현

--- src/main/java/com/devnest/board/controller/SearchController.java
+++ src/main/java/com/devnest/board/controller/SearchController.java
@@ -2,7 +2,6 @@ package com.devnest.board.controller;
 
 import com.devnest.board.dto.TopicResponseDTO;
 import com.devnest.board.service.SearchService;
-import com.devnest.board.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -18,12 +17,27 @@ public class SearchController {
     private SearchService searchService;
 
     @GetMapping
-    public String search(@RequestParam String keyword,
+    public String search(@RequestParam(required = false, defaultValue = "") String keyword,
+                         @RequestParam(required = false, defaultValue = "") String tag,
                          @RequestParam(required = false, defaultValue = "false") boolean resolved,
                          @RequestParam(required = false, defaultValue="0") int page,
                          Model model){
-        Page<TopicResponseDTO> searchResult = searchService.getSearchResult(keyword, page, resolved);
+        Page<TopicResponseDTO> searchResult;
+
+        if  (keyword.isEmpty() && tag.isEmpty()){
+            return "redirect:/topics";
+        }else if(keyword.isEmpty()){
+            searchResult = searchService.getTagSearchResult(tag,page,resolved);
+        }else if(tag.isEmpty()){
+            searchResult = searchService.getKeywordSearchResult(keyword, page, resolved);
+        }else{
+            searchResult = searchService.getKeywordAndTagSearchResult(keyword,tag,page,resolved);
+        }
+
         model.addAttribute("topics", searchResult);
+        model.addAttribute("keyword",keyword);
+        model.addAttribute("tag",tag);
+        model.addAttribute( "resolved",resolved);
 
         return "search";
     }
--- src/main/java/com/devnest/board/service/SearchService.java
+++ src/main/java/com/devnest/board/service/SearchService.java
@@ -22,12 +22,12 @@ public class SearchService {
         this.topicRepository = topicRepository;
     }
 
-    public Page<TopicResponseDTO> getSearchResult(String keyword, int page, boolean resolved){
+    public Page<TopicResponseDTO> getKeywordSearchResult(String keyword, int page, boolean resolved) {
         Pageable pageable = PageRequest.of(page, 7);
         List<TopicResponseDTO> responseDTOS = new ArrayList<>();
 
-        Page<BoardTopic> topics = resolved ? topicRepository.searchByKeyword(pageable, keyword)
-                                            : topicRepository.searchSolvedByKeyword(pageable,keyword);
+        Page<BoardTopic> topics = resolved ? topicRepository.searchSolvedByKeyword(pageable, keyword)
+                : topicRepository.searchByKeyword(pageable, keyword);
 
         for (BoardTopic topic : topics) {
             responseDTOS.add(new TopicResponseDTO(topic));
@@ -35,4 +35,33 @@ public class SearchService {
 
         return new PageImpl<>(responseDTOS, pageable, topics.getTotalElements());
     }
+
+    public Page<TopicResponseDTO> getTagSearchResult(String tag, int page, boolean resolved) {
+        Pageable pageable = PageRequest.of(page, 7);
+        List<TopicResponseDTO> responseDTOS = new ArrayList<>();
+
+        Page<BoardTopic> topics = resolved ? topicRepository.searchSolvedByTag(pageable, tag)
+                : topicRepository.searchByTag(pageable, tag);
+
+        for (BoardTopic topic : topics) {
+            responseDTOS.add(new TopicResponseDTO(topic));
+        }
+
+        return new PageImpl<>(responseDTOS, pageable, topics.getTotalElements());
+    }
+
+    public Page<TopicResponseDTO> getKeywordAndTagSearchResult(String keyword, String tag, int page, boolean resolved) {
+        Pageable pageable = PageRequest.of(page, 7);
+        List<TopicResponseDTO> responseDTOS = new ArrayList<>();
+
+        Page<BoardTopic> topics = resolved ? topicRepository.searchSolvedByKeywordAndTag(pageable, keyword, tag)
+                : topicRepository.searchByKeywordAndTag(pageable, keyword, tag);
+
+        for (BoardTopic topic : topics) {
+            responseDTOS.add(new TopicResponseDTO(topic));
+        }
+
+        return new PageImpl<>(responseDTOS, pageable, topics.getTotalElements());
+    }
+
 }
