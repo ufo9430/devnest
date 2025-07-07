7f1795a970042f462ad0de321174ff28d9da3dc4
Lee Chae Rin <chaerin0916@gmail.com>
2025-07-08 00:14:54 +0900

    fix: pattern 수정

---
+++ src/main/java/com/devnest/global/advice/CsrfAdvice.java
@@ -0,0 +1,4 @@
+package com.devnest.global.advice;
+
+public class CsrfAdvice {
+}
--- src/main/java/com/devnest/user/dto/request/PasswordChangeRequestDto.java
+++ src/main/java/com/devnest/user/dto/request/PasswordChangeRequestDto.java
@@ -14,8 +14,8 @@ public class PasswordChangeRequestDto {
 
     @NotBlank(message = "새 비밀번호를 입력하세요.")
     @Pattern(
-            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
-            message = "비밀번호는 영문, 숫자, 특수문자를 포함한 8자 이상이어야 합니다."
+            regexp = "^[A-Za-z\\d~!@#$%^&*()_+\\-=\\[\\]{},.<>?/]{8,}$",
+            message = "비밀번호는 8자 이상이어야 합니다."
     )
     private String newPassword;
 
