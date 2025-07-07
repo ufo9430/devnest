e60ac078a2d0a98bda52308a2f837e414534152b
Lee Chae Rin <chaerin0916@gmail.com>
2025-07-08 00:16:09 +0900

    feat: 컨트롤러에 csrf 주입 위해 CsrfAdvice 구현

--- src/main/java/com/devnest/global/advice/CsrfAdvice.java
+++ src/main/java/com/devnest/global/advice/CsrfAdvice.java
@@ -1,4 +1,15 @@
 package com.devnest.global.advice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+// 모든 컨트롤러에 _csrf 자동 주입
+@ControllerAdvice
 public class CsrfAdvice {
+  @ModelAttribute("_csrf")
+  public CsrfToken csrfToken(HttpServletRequest request) {
+    return (CsrfToken) request.getAttribute("_csrf");
+  }
 }
