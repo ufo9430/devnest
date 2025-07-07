d3d38157b271bec87c49a899f4674dc758532a43
ufo9430 <ufo9430@naver.com>
2025-07-07 12:44:57 +0900

    feat: 인기 게시글 등록 기능 구현

--- src/main/java/com/devnest/board/domain/HotTopic.java
+++ src/main/java/com/devnest/board/domain/HotTopic.java
@@ -2,15 +2,21 @@ package com.devnest.board.domain;
 
 import com.devnest.topic.domain.Topic;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class HotTopic {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private LocalDateTime snapshotTime;
